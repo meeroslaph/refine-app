6229551a18e234d84a1dcd3311874e59a8470e2f
meeroslaph <6173298+meeroslaph@users.noreply.github.com>
2025-02-04 21:43:04 +0100

    Add login routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Refine, Authenticated } from "@refinedev/core";
 import routerProvider from "@refinedev/react-router";
 
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router";
 
 import { dataProvider } from "./providers/data-provider";
 import { authProvider } from "./providers/auth-provider";
@@ -19,13 +19,31 @@ export default function App(): JSX.Element {
         authProvider={authProvider}
         routerProvider={routerProvider}
       >
-        <Authenticated key="protected" fallback={<Login />}>
-          <Header />
-          {/* <ShowProduct /> */}
-          {/* <EditProduct /> */}
-          <ListProducts />
-          {/* <CreateProduct /> */}
-        </Authenticated>
+        <Routes>
+          <Route
+            element={
+              // We're wrapping our routes with the `<Authenticated />` component
+              // We're omitting the `fallback` prop to redirect users to the login page if they are not authenticated.
+              // If the user is authenticated, we'll render the `<Header />` component and the `<Outlet />` component to render the inner routes.
+              <Authenticated key="authenticated-routes" redirectOnFail="/login">
+                <Header />
+                <Outlet />
+              </Authenticated>
+            }
+          >
+            <Route index element={<ListProducts />} />
+          </Route>
+          <Route
+            element={
+              <Authenticated key="auth-pages" fallback={<Outlet />}>
+                {/* We're redirecting the user to `/` if they are authenticated and trying to access the `/login` route */}
+                <Navigate to="/" />
+              </Authenticated>
+            }
+          >
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
       </Refine>
     </BrowserRouter>
   );
