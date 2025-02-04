1f8ccd4822bd3576ae670219bc93ad2d34fa57dd
meeroslaph <6173298+meeroslaph@users.noreply.github.com>
2025-02-04 18:26:51 +0100

    Add authentication and logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Refine, WelcomePage } from "@refinedev/core";
+import { Refine, Authenticated } from "@refinedev/core";
 
-function App() {
+import { dataProvider } from "./providers/data-provider";
+import { authProvider } from "./providers/auth-provider";
+
+import { ListProducts } from "./pages/products/list";
+
+import { Login } from "./pages/login";
+import { Header } from "./components/header";
+
+export default function App(): JSX.Element {
   return (
-    <Refine>
-      <WelcomePage />
+    <Refine
+        dataProvider={dataProvider}
+        authProvider={authProvider}
+    >
+      <Authenticated
+        key="protected"
+        fallback={<Login />}
+      >
+        <Header />
+        {/* <ShowProduct /> */}
+        {/* <EditProduct /> */}
+        <ListProducts />
+        {/* <CreateProduct /> */}
+      </Authenticated>
     </Refine>
   );
 }
-
-export default App;
