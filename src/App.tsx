0d071c24454e2f7dacf512e03c1891a3fc7bebbc
meeroslaph <6173298+meeroslaph@users.noreply.github.com>
2025-02-04 21:50:16 +0100

    Define resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Refine, Authenticated } from "@refinedev/core";
-import routerProvider from "@refinedev/react-router";
+import routerProvider, { NavigateToResource } from "@refinedev/react-router";
 
-import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router";
 
 import { dataProvider } from "./providers/data-provider";
 import { authProvider } from "./providers/auth-provider";
 
+import { ShowProduct } from "./pages/products/show";
+import { EditProduct } from "./pages/products/edit";
 import { ListProducts } from "./pages/products/list";
+import { CreateProduct } from "./pages/products/create";
 
 import { Login } from "./pages/login";
 import { Header } from "./components/header";
@@ -18,26 +21,45 @@ export default function App(): JSX.Element {
         dataProvider={dataProvider}
         authProvider={authProvider}
         routerProvider={routerProvider}
+        resources={[
+          {
+            name: "protected-products",
+            list: "/products",
+            show: "/products/:id",
+            edit: "/products/:id/edit",
+            create: "/products/create",
+            meta: { label: "Products" },
+          },
+        ]}
       >
         <Routes>
           <Route
             element={
-              // We're wrapping our routes with the `<Authenticated />` component
-              // We're omitting the `fallback` prop to redirect users to the login page if they are not authenticated.
-              // If the user is authenticated, we'll render the `<Header />` component and the `<Outlet />` component to render the inner routes.
               <Authenticated key="authenticated-routes" redirectOnFail="/login">
                 <Header />
                 <Outlet />
               </Authenticated>
             }
           >
-            <Route index element={<ListProducts />} />
+            <Route
+              index
+              // We're also replacing the <Navigate /> component with the <NavigateToResource /> component.
+              // It's tailored version of the <Navigate /> component that will redirect to the resource's list route.
+              element={<NavigateToResource resource="protected-products" />}
+            />
+            <Route path="/products">
+              <Route index element={<ListProducts />} />
+              <Route path=":id" element={<ShowProduct />} />
+              <Route path=":id/edit" element={<EditProduct />} />
+              <Route path="create" element={<CreateProduct />} />
+            </Route>
           </Route>
           <Route
             element={
               <Authenticated key="auth-pages" fallback={<Outlet />}>
-                {/* We're redirecting the user to `/` if they are authenticated and trying to access the `/login` route */}
-                <Navigate to="/" />
+                {/* We're also replacing the <Navigate /> component with the <NavigateToResource /> component. */}
+                {/* It's tailored version of the <Navigate /> component that will redirect to the resource's list route. */}
+                <NavigateToResource resource="protected-products" />
               </Authenticated>
             }
           >
