d2e673c48ee2c5e62e76baca26ecfd94dca4b755
meeroslaph <6173298+meeroslaph@users.noreply.github.com>
2025-02-05 11:50:48 +0100

    Add CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import routerProvider, { NavigateToResource } from "@refinedev/react-router";
 
 import { BrowserRouter, Routes, Route, Outlet } from "react-router";
 
+import { RefineThemes, ThemedLayoutV2 } from "@refinedev/mui";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import { ThemeProvider } from "@mui/material/styles";
+
 import { dataProvider } from "./providers/data-provider";
 import { authProvider } from "./providers/auth-provider";
 
@@ -12,61 +18,64 @@ import { ListProducts } from "./pages/products/list";
 import { CreateProduct } from "./pages/products/create";
 
 import { Login } from "./pages/login";
-import { Header } from "./components/header";
 
 export default function App(): JSX.Element {
   return (
     <BrowserRouter>
-      <Refine
-        dataProvider={dataProvider}
-        authProvider={authProvider}
-        routerProvider={routerProvider}
-        resources={[
-          {
-            name: "protected-products",
-            list: "/products",
-            show: "/products/:id",
-            edit: "/products/:id/edit",
-            create: "/products/create",
-            meta: { label: "Products" },
-          },
-        ]}
-      >
-        <Routes>
-          <Route
-            element={
-              <Authenticated key="authenticated-routes" redirectOnFail="/login">
-                <Header />
-                <Outlet />
-              </Authenticated>
-            }
-          >
+      <ThemeProvider theme={RefineThemes.Blue}>
+        <CssBaseline />
+        <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+        <Refine
+          dataProvider={dataProvider}
+          authProvider={authProvider}
+          routerProvider={routerProvider}
+          resources={[
+            {
+              name: "protected-products",
+              list: "/products",
+              show: "/products/:id",
+              edit: "/products/:id/edit",
+              create: "/products/create",
+              meta: { label: "Products" },
+            },
+          ]}
+        >
+          <Routes>
             <Route
-              index
-              // We're also replacing the <Navigate /> component with the <NavigateToResource /> component.
-              // It's tailored version of the <Navigate /> component that will redirect to the resource's list route.
-              element={<NavigateToResource resource="protected-products" />}
-            />
-            <Route path="/products">
-              <Route index element={<ListProducts />} />
-              <Route path=":id" element={<ShowProduct />} />
-              <Route path=":id/edit" element={<EditProduct />} />
-              <Route path="create" element={<CreateProduct />} />
+              element={
+                <Authenticated
+                  key="authenticated-routes"
+                  redirectOnFail="/login"
+                >
+                  <ThemedLayoutV2>
+                    <Outlet />
+                  </ThemedLayoutV2>
+                </Authenticated>
+              }
+            >
+              <Route
+                index
+                element={<NavigateToResource resource="protected-products" />}
+              />
+              <Route path="/products">
+                <Route index element={<ListProducts />} />
+                <Route path=":id" element={<ShowProduct />} />
+                <Route path=":id/edit" element={<EditProduct />} />
+                <Route path="create" element={<CreateProduct />} />
+              </Route>
             </Route>
-          </Route>
-          <Route
-            element={
-              <Authenticated key="auth-pages" fallback={<Outlet />}>
-                {/* We're also replacing the <Navigate /> component with the <NavigateToResource /> component. */}
-                {/* It's tailored version of the <Navigate /> component that will redirect to the resource's list route. */}
-                <NavigateToResource resource="protected-products" />
-              </Authenticated>
-            }
-          >
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-      </Refine>
+            <Route
+              element={
+                <Authenticated key="auth-pages" fallback={<Outlet />}>
+                  <NavigateToResource resource="protected-products" />
+                </Authenticated>
+              }
+            >
+              <Route path="/login" element={<Login />} />
+            </Route>
+          </Routes>
+        </Refine>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
