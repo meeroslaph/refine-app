4b4ba089fb9aa6fc19b103b5b63391974cfec6c5
meeroslaph <6173298+meeroslaph@users.noreply.github.com>
2025-02-05 17:30:17 +0100

    Install Refine Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { DevtoolsProvider, DevtoolsPanel } from "@refinedev/devtools";
 import { Refine, Authenticated } from "@refinedev/core";
 import routerProvider, { NavigateToResource } from "@refinedev/react-router";
 
@@ -29,82 +30,91 @@ import { Login } from "./pages/login";
 
 export default function App(): JSX.Element {
   return (
-    (<BrowserRouter>
+    <BrowserRouter>
       <ThemeProvider theme={RefineThemes.Blue}>
         <CssBaseline />
         <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
         <RefineSnackbarProvider>
-          <Refine
-            dataProvider={dataProvider}
-            authProvider={authProvider}
-            routerProvider={routerProvider}
-            notificationProvider={useNotificationProvider}
-            resources={[{
-              name: "protected-products",
-              list: "/products",
-              show: "/products/:id",
-              edit: "/products/:id/edit",
-              create: "/products/create",
-              meta: { label: "Products" },
-            }, {
-              name: "categories",
-              list: "/categories",
-              meta: { label: "Categories" },
-            }, {
-              name: "events",
-              list: "/events",
-              create: "/events/create",
-              edit: "/events/edit/:id",
-              show: "/events/show/:id"
-            }]}
-          >
-            <Routes>
-              <Route
-                element={
-                  <Authenticated
-                    key="authenticated-routes"
-                    redirectOnFail="/login"
-                  >
-                    <ThemedLayoutV2
-                      Title={(props) => (
-                        <ThemedTitleV2 {...props} text="Awesome Project" />
-                      )}
-                    >
-                      <Outlet />
-                    </ThemedLayoutV2>
-                  </Authenticated>
-                }
-              >
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              authProvider={authProvider}
+              routerProvider={routerProvider}
+              notificationProvider={useNotificationProvider}
+              resources={[
+                {
+                  name: "protected-products",
+                  list: "/products",
+                  show: "/products/:id",
+                  edit: "/products/:id/edit",
+                  create: "/products/create",
+                  meta: { label: "Products" },
+                },
+                {
+                  name: "categories",
+                  list: "/categories",
+                  meta: { label: "Categories" },
+                },
+                {
+                  name: "events",
+                  list: "/events",
+                  create: "/events/create",
+                  edit: "/events/edit/:id",
+                  show: "/events/show/:id",
+                },
+              ]}
+            >
+              <Routes>
                 <Route
-                  index
-                  element={<NavigateToResource resource="protected-products" />}
-                />
-                <Route path="/products">
-                  <Route index element={<ListProducts />} />
-                  <Route path=":id" element={<ShowProduct />} />
-                  <Route path=":id/edit" element={<EditProduct />} />
-                  <Route path="create" element={<CreateProduct />} />
+                  element={
+                    <Authenticated
+                      key="authenticated-routes"
+                      redirectOnFail="/login"
+                    >
+                      <ThemedLayoutV2
+                        Title={(props) => (
+                          <ThemedTitleV2 {...props} text="Awesome Project" />
+                        )}
+                      >
+                        <Outlet />
+                      </ThemedLayoutV2>
+                    </Authenticated>
+                  }
+                >
+                  <Route
+                    index
+                    element={
+                      <NavigateToResource resource="protected-products" />
+                    }
+                  />
+                  <Route path="/products">
+                    <Route index element={<ListProducts />} />
+                    <Route path=":id" element={<ShowProduct />} />
+                    <Route path=":id/edit" element={<EditProduct />} />
+                    <Route path="create" element={<CreateProduct />} />
+                  </Route>
+                  <Route path="/categories">
+                    <Route index element={<ListCategories />} />
+                  </Route>
+                  <Route path="/events">
+                    <Route index element={<ListCategories />} />
+                  </Route>
                 </Route>
-                <Route path="/categories">
-                  <Route index element={<ListCategories />} />
+                <Route
+                  element={
+                    <Authenticated key="auth-pages" fallback={<Outlet />}>
+                      <NavigateToResource resource="protected-products" />
+                    </Authenticated>
+                  }
+                >
+                  <Route path="/login" element={<Login />} />
                 </Route>
-                <Route path="/events">
-                  <Route index element={<ListCategories />} />
-                </Route>
-              </Route>
-              <Route
-                element={
-                  <Authenticated key="auth-pages" fallback={<Outlet />}>
-                    <NavigateToResource resource="protected-products" />
-                  </Authenticated>
-                }
-              >
-                <Route path="/login" element={<Login />} />
-              </Route>
-            </Routes>
-          </Refine>
+              </Routes>
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
         </RefineSnackbarProvider>
       </ThemeProvider>
-    </BrowserRouter>)
+    </BrowserRouter>
   );
 }
